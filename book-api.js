ea0e79c96e1f24e9f9af2cf669af3736879ed124
taxjames123@gmail.com <taxjames123@gmail.com>
2023-05-16 15:14:49 -0700

    files for lab5

---
+++ book-api.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express();
+const port = 3000;
+
+let books = [];
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/book', (req, res) => {
+    const book = req.body;
+
+    console.log(book);
+    books.push(book);
+
+    res.send('Book is added to the database');
+});
+
+app.get('/books', (req, res) => {
+    res.json(books);
+});
+
+app.post('/book/:isbn', (req, res) => {
+    const isbn = req.params.isbn;
+    const newBook = req.body;
+
+    for (let i = 0; i < books.length; i++){
+        let book = books[i]
+
+        if (book.isbn === isbn){
+            books[i] = newBook;
+        }
+    }
+
+    res.send('Book is edited');
+});
+
+app.delete('/book/:isbn', (req, res) => {
+    const isbn = req.params.isbn;
+
+    books = books.filter((book) => book.isbn !== isbn);
+
+    res.send('Book is deleted');
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
---
+++ book-list.js
@@ -0,0 +1,77 @@
+async function loadBooks (){
+    let response = await fetch("http://localhost:3000/books");
+
+    console.log(response.status);
+    console.log(response.statusText);
+
+    if (response.status === 200){
+        let data = await response.text();
+        console.log(data);
+        const books = JSON.parse(data);
+
+        const{
+            title,
+            author,
+            publisher,
+            publish_date,
+            numOfPages,
+        } = books;
+
+        document.getElementById('isbn').value = isbn;
+        document.getElementById('title').value = title;
+        document.getElementById('author').value = author;
+        document.getElementById('publisher').value = publisher;
+        document.getElementById('publish_date').value = publish_date;
+        document.getElementById('numOfPages').value = numOfPages;
+
+        document.getElementById('editForm').action = 'http://localhost:3000/book/${isbn}';
+
+
+        for (let book of books){
+            const x = `
+                <div class="col-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">${book.title}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">${book.isbn}</h6>
+
+                            <div>Author: ${book.author}</div>
+                            <div>Publisher: ${book.publisher}</div>
+                            <div>Number of Pages: ${book.numOfPages}</div>
+
+                            <hr>
+
+                            <button type="button" class="btn btn-danger">Delete</button>
+                            <button types="button" class="btn btn-primary" data-toggle="modal"
+                                data-target="#editBookModal" onclick="setEditModal(${book.isbn})">
+                                Edit
+                            </button>
+                            <script>
+                                const deleteButton = document.getElementById('deleteButton');
+                                deleteButton.addEventListener('click', deleteBook);
+                                function deleteBook(){
+                                    const isbn = ${book.isbn};
+                                    const deleteRequest = new XMLHttpRequest();
+                                    deleteRequest.open('DELETE', '/books/' + isbn);
+                                    deleteRequest.onreadystatechange = function () {
+                                        if (deleteRequest.readyState === 4 && deleteRequest.status === 200) {
+                                            // Book deleted successfully, you can update the UI as needed
+                                            console.log('Book deleted');
+                                        } else {
+                                            console.log('Error deleting book');
+                                        }
+                                    };
+                                    deleteRequest.send();
+                                }
+                            </script>
+                        </div>
+                    </div>
+                </div>
+            `
+
+            document.getElementById("books").innerHTML = document.getElementById("books").innerHTML + x;
+        }
+    }
+}
+
+loadBooks();
